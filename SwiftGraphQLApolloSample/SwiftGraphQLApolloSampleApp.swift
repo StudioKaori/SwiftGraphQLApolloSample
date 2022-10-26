afc08df7a3be0a2449a6abb06508f020ae8a4ac9
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-10-26 09:48:47 +0200

    Initial Commit

---
+++ SwiftGraphQLApolloSample/SwiftGraphQLApolloSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGraphQLApolloSampleApp.swift
+//  SwiftGraphQLApolloSample
+//
+//  Created by Kaori Persson on 2022-10-26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftGraphQLApolloSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
